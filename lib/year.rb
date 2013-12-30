a9515e1795de769fe1f4fad0828b4351355c72d0
Amanda Thurman <AmandaThurman@Amandas-MacBook-Pro.local>
2013-12-30 11:45:27 -0600

    new method to create month array, last test not passing, all months printing

--- lib/year.rb
+++ lib/year.rb
@@ -7,36 +7,37 @@ class Year
     @year = year
   end
 
-  def print
-    month1 = Month.new(1, @year)
-    month_ary1 = month1.month_string
-    ary1 = month_ary1.split("\n")
-    ary1.insert(0, "January".center(20))
-    ary1.insert(1, "Su Mo Tu We Th Fr Sa")
-    last_week = ary1[6]
+  def create_month_array month
+    month_names = {1 => "January", 2 => "February", 3 => "March", 4 => "April",
+                   5 => "May", 6 => "June", 7 => "July", 8 => "August",
+                   9 => "September", 10 => "October", 11 => "November", 12 => "December"}
+    new_month = Month.new(month, @year)
+    month_ary = new_month.month_string
+    ary = month_ary.split("\n")
+    ary.insert(0, month_names[month].center(20))
+    ary.insert(1, "Su Mo Tu We Th Fr Sa")
+    last_week = ary[6]
     until last_week.size == 20 do
       x = "   "
       last_week = last_week + x
     end
-    ary1[6].replace(last_week)
+    ary[6].replace(last_week)
+    return ary
+  end
 
-    month2 = Month.new(2, @year)
-    month_ary2 = month2.month_string
-    ary2 = month_ary2.split("\n")
-    ary2.insert(0, "February".center(20))
-    ary2.insert(1, "Su Mo Tu We Th Fr Sa")
-    last_week2 = ary2[6]
-    until last_week2.size == 20 do
-      x = "   "
-      last_week2 = last_week2 + x
-    end
-    ary2[6].replace(last_week2)
-
-    month3 = Month.new(3, @year)
-    month_ary3 = month3.month_string
-    ary3 = month_ary3.split("\n")
-    ary3.insert(0, "March".center(20))
-    ary3.insert(1, "Su Mo Tu We Th Fr Sa")
+  def print
+    ary1 = create_month_array(1)
+    ary2 = create_month_array(2)
+    ary3 = create_month_array(3)
+    ary4 = create_month_array(4)
+    ary5 = create_month_array(5)
+    ary6 = create_month_array(6)
+    ary7 = create_month_array(7)
+    ary8 = create_month_array(8)
+    ary9 = create_month_array(9)
+    ary10 = create_month_array(10)
+    ary11 = create_month_array(11)
+    ary12 = create_month_array(12)
 
     puts "#{@year}".center(62).rstrip
     puts
@@ -48,6 +49,29 @@ class Year
     puts ary1[5] + "  " + ary2[5] + "  " + ary3[5].rstrip
     puts ary1[6] + "  " + ary2[6] + "  " + ary3[6].rstrip
     puts
+    puts ary4[0] + "  " + ary5[0] + "  " + ary6[0].rstrip
+    puts ary4[1] + "  " + ary5[1] + "  " + ary6[1].rstrip
+    puts ary4[2] + "  " + ary5[2] + "  " + ary6[2].rstrip
+    puts ary4[3] + "  " + ary5[3] + "  " + ary6[3].rstrip
+    puts ary4[4] + "  " + ary5[4] + "  " + ary6[4].rstrip
+    puts ary4[5] + "  " + ary5[5] + "  " + ary6[5].rstrip
+    puts ary4[6] + "  " + ary5[6] + "  " + ary6[6].rstrip
+    puts
+    puts ary7[0] + "  " + ary8[0] + "  " + ary9[0].rstrip
+    puts ary7[1] + "  " + ary8[1] + "  " + ary9[1].rstrip
+    puts ary7[2] + "  " + ary8[2] + "  " + ary9[2].rstrip
+    puts ary7[3] + "  " + ary8[3] + "  " + ary9[3].rstrip
+    puts ary7[4] + "  " + ary8[4] + "  " + ary9[4].rstrip
+    puts ary7[5] + "  " + ary8[5] + "  " + ary9[5].rstrip
+    puts ary7[6] + "  " + ary8[6] + "  " + ary9[6].rstrip
+    puts
+    puts ary10[0] + "  " + ary11[0] + "  " + ary12[0].rstrip
+    puts ary10[1] + "  " + ary11[1] + "  " + ary12[1].rstrip
+    puts ary10[2] + "  " + ary11[2] + "  " + ary12[2].rstrip
+    puts ary10[3] + "  " + ary11[3] + "  " + ary12[3].rstrip
+    puts ary10[4] + "  " + ary11[4] + "  " + ary12[4].rstrip
+    puts ary10[5] + "  " + ary11[5] + "  " + ary12[5].rstrip
+    puts ary10[6] + "  " + ary11[6] + "  " + ary12[6].rstrip
   end
 end
 
