68df5b8f2906254aba3f00f408f5fbe3e05e62fb
Amanda Thurman <AmandaThurman@Amandas-MacBook-Pro.local>
2013-12-31 09:59:09 -0600

    year input for cal.rb succesfully passing in comparison to unix cal

--- lib/year.rb
+++ lib/year.rb
@@ -16,12 +16,28 @@ class Year
     ary = month_ary.split("\n")
     ary.insert(0, month_names[month].center(20))
     ary.insert(1, "Su Mo Tu We Th Fr Sa")
-    last_week = ary[6]
-    until last_week.size == 20 do
-      x = "   "
-      last_week = last_week + x
+    if ary.length < 8
+      last_week = ary[6]
+      until last_week.size == 20 do
+        x = "   "
+        last_week = last_week + x
+      end
+      ary[6].replace(last_week)
+      end_of_month = "  "
+      until end_of_month.size == 20 do
+        x = "  "
+        end_of_month = end_of_month + x
+      end
+      ary.push(end_of_month)
+    else
+      last_week = ary[7]
+      until last_week.size == 20 do
+        x = "   "
+        last_week = last_week + x
+      end
+      ary[7].replace(last_week)
     end
-    ary[6].replace(last_week)
+
     return ary
   end
 
@@ -48,7 +64,7 @@ class Year
     puts ary1[4] + "  " + ary2[4] + "  " + ary3[4].rstrip
     puts ary1[5] + "  " + ary2[5] + "  " + ary3[5].rstrip
     puts ary1[6] + "  " + ary2[6] + "  " + ary3[6].rstrip
-    puts
+    puts ary1[7] + "  " + ary2[7] + "  " + ary3[7].rstrip
     puts ary4[0] + "  " + ary5[0] + "  " + ary6[0].rstrip
     puts ary4[1] + "  " + ary5[1] + "  " + ary6[1].rstrip
     puts ary4[2] + "  " + ary5[2] + "  " + ary6[2].rstrip
@@ -56,7 +72,7 @@ class Year
     puts ary4[4] + "  " + ary5[4] + "  " + ary6[4].rstrip
     puts ary4[5] + "  " + ary5[5] + "  " + ary6[5].rstrip
     puts ary4[6] + "  " + ary5[6] + "  " + ary6[6].rstrip
-    puts
+    puts ary4[7] + "  " + ary5[7] + "  " + ary6[7].rstrip
     puts ary7[0] + "  " + ary8[0] + "  " + ary9[0].rstrip
     puts ary7[1] + "  " + ary8[1] + "  " + ary9[1].rstrip
     puts ary7[2] + "  " + ary8[2] + "  " + ary9[2].rstrip
@@ -64,7 +80,7 @@ class Year
     puts ary7[4] + "  " + ary8[4] + "  " + ary9[4].rstrip
     puts ary7[5] + "  " + ary8[5] + "  " + ary9[5].rstrip
     puts ary7[6] + "  " + ary8[6] + "  " + ary9[6].rstrip
-    puts
+    puts ary7[7] + "  " + ary8[7] + "  " + ary9[7].rstrip
     puts ary10[0] + "  " + ary11[0] + "  " + ary12[0].rstrip
     puts ary10[1] + "  " + ary11[1] + "  " + ary12[1].rstrip
     puts ary10[2] + "  " + ary11[2] + "  " + ary12[2].rstrip
@@ -72,6 +88,7 @@ class Year
     puts ary10[4] + "  " + ary11[4] + "  " + ary12[4].rstrip
     puts ary10[5] + "  " + ary11[5] + "  " + ary12[5].rstrip
     puts ary10[6] + "  " + ary11[6] + "  " + ary12[6].rstrip
+    puts ary10[7] + "  " + ary11[7] + "  " + ary12[7].rstrip
   end
 end
 
