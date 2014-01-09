6377f33aedffdbb4ebc944c6330cd17a938567f7
amanda-thrive <amanda@thrivemg.com>
2014-01-09 15:35:45 -0600

    move initialize function and remove extra lines in month class

--- lib/month.rb
+++ lib/month.rb
@@ -1,7 +1,11 @@
 require_relative 'zellers_congruence'
 
 class Month
-  attr_reader :month_string
+
+  def initialize month, year
+    @month = month
+    @year = year
+  end
 
   def is_leapyear?
     if @year % 100 == 0 && @year % 4 == 0
@@ -59,11 +63,6 @@ class Month
     {1 => "January", 2 => "February", 3 => "March", 4 => "April",
      5 => "May", 6 => "June", 7 => "July", 8 => "August",
      9 => "September", 10 => "October", 11 => "November", 12 => "December"}
-  end
-
-  def initialize month, year
-    @month = month
-    @year = year
   end
 
   def month_array
